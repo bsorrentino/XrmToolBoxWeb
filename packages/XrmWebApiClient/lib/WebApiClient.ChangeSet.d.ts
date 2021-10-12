357419194052c2afe9e3bac0adc96aa059373f7a
bsorrentino <bartolomeo.sorrentino@gmail.com>
2021-10-12 16:53:22 +0200

    update

--- packages/XrmWebApiClient/lib/WebApiClient.ChangeSet.d.ts
+++
@@ -1,7 +0,0 @@
-import { ChangeSetParameters, IBatchRequest, IChangeSet } from "./WebApiClient.Base";
-export declare class ChangeSet implements IChangeSet {
-    name: string;
-    requests: Array<IBatchRequest>;
-    constructor(params: ChangeSetParameters);
-    stringify(): string;
-}
