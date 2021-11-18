1015a6ba37b43051c05af74110ef4230968480bc
bsorrentino <bartolomeo.sorrentino@gmail.com>
2021-11-18 17:56:10 +0100

    update(code): show environment url

--- packages/SystemForms/src/index.tsx
+++ packages/SystemForms/src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 
 import { MsalProvider } from "@azure/msal-react";
 
-import { PCA } from "xrmtoolboxweb-core";
+import { PCA , scopes as WebApiScopes } from "xrmtoolboxweb-core";
 import {App} from "./app";
 
 // Component
 const AppProvider = () => (
     <MsalProvider instance={PCA}>
+        <h3>Scope: {WebApiScopes[0]}</h3>
+        <hr/>
         <App />
     </MsalProvider>
 );
--- packages/core/src/webapi.ts
+++ packages/core/src/webapi.ts
@@ -2,20 +2,23 @@ import { AuthenticationResult } from "@azure/msal-browser";
 import * as WebApiClient from 'xrmtoolboxweb-webapiclient'
 
 
-const POWERPLATFORM_ENV_URL = [
-    'https://bsc-labs.crm.dynamics.com', 
-    'https://org8b3e7e60.crm4.dynamics.com'
-]
 
+const CurrentEnv =  () => {
+    const POWERPLATFORM_ENV_URL = [
+        'https://bsc-labs.crm.dynamics.com', 
+        'https://org8b3e7e60.crm4.dynamics.com'
+    ]
+    return POWERPLATFORM_ENV_URL[1]
+}
 
-export const scopes = [ `${POWERPLATFORM_ENV_URL[1]}/user_impersonation` ]
+export const scopes = [ `${CurrentEnv()}/user_impersonation` ]
 
 export const prepareWebApiRequest = ( auth:AuthenticationResult ) => {
     
     let client = WebApiClient.Instance
     client.ApiVersion = '9.1'
     client.Token = auth.accessToken
-    client.ClientUrl = POWERPLATFORM_ENV_URL[1]
+    client.ClientUrl = CurrentEnv()
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
